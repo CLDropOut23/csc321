73473c7a768d02458840cea5b0b34cddbe8df216
Ryan Rodriguez <rrodriguez439@toromail.csudh.edu>
2026-02-25 02:34:43 +0000

    CSC321 Lab#7

---
+++ rrodriguez7.py
@@ -0,0 +1,29 @@
+#Ryan Rodriguez
+#CSC321
+#Lab7
+#2/24/26
+
+x = 23
+y = 19.83
+z = "Dominguez"
+
+if x > 24:
+    print("X is greater than 23")
+elif x < 24:
+    print("X is less than 24 and the value of x is ",x)
+else:
+    print("X is ",x," which is equal to 24")
+    
+if y < 19.83:
+    print("y is less than 19.83")
+elif y == 19.83:
+    print("y is equal to 19.83")
+elif y > 19.83:
+    print("y is greater than 19.83")
+else:
+    print("x is between 19.80 and 19.90")
+
+if z == "Dominguez":
+    print("z is Dominguez")
+else:
+    print("z is not Dominguez")
